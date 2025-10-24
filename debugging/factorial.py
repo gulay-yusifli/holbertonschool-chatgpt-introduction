b257d00d4c7155f7ed10a4aef911e751f13ba468
gulay-yusifli <yusifligulay06@gmail.com>
2025-10-24 14:58:28 +0000

    task

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,25 +2,11 @@
 import sys
 
 def factorial(n):
-    result = 1
-    while n > 1:
-        result *= n
-        n -= 1   # Decrement n to avoid infinite loop
-    return result
+	result = 1
+	while n > 1:
+		result *= n
+		n -= 1
+	return result
 
-if len(sys.argv) < 2:
-    print("Usage: python3 factorial.py <number>")
-    sys.exit(1)
-
-try:
-    n = int(sys.argv[1])
-    if n < 0:
-        print("Error: Factorial is not defined for negative numbers.")
-        sys.exit(1)
-except ValueError:
-    print("Error: Argument must be an integer.")
-    sys.exit(1)
-
-f = factorial(n)
+f = factorial(int(sys.argv[1]))
 print(f)
-
